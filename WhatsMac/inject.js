b0543fb75150982f3940938a319aa0a935279c58
Sam Stone <stonesam92@gmail.com>
2015-05-22 12:40:56 +0100

    Make JS style more consistent

--- WhatsMac/inject.js
+++ WhatsMac/inject.js
@@ -12,7 +12,7 @@ var styleAdditions = document.createElement('style');
 styleAdditions.textContent = 'div.pane-list-user {opacity:0;} \
 div.app-wrapper::before {opacity: 0;} \
 div.drawer-title {left:60px; bottom:17px;} \
-div[class="chat media-chat"] > div.chat-avatar { opacity: 0; } \
+div.chat.media-chat > div.chat-avatar { opacity: 0; } \
 div.app.two { top: 0px; width: 100%; height: 100%; } \
 div.app.three { top: 0px; width: 100%; height: 100%; } \
 div.pane.pane-chat { width : 100%; } \
@@ -21,26 +21,26 @@ div.pane.pane-intro { width : 100%; } \
 document.documentElement.appendChild(styleAdditions);
 
 function activateSearchField() {
-    document.querySelector('input[class~=\"input-search\"]').focus();
+    document.querySelector('input.input-search').focus();
 }
 
 function newConversation() {
-    document.querySelector('button[class~=\"icon-chat\"]').click();
-    document.querySelector('input[class~=\"input-search\"]').focus();
+    document.querySelector('button.icon-chat').click();
+    document.querySelector('input.input-search').focus();
     
-    var header=document.querySelector("div[class~='drawer-title']");
+    var header=document.querySelector('div.drawer-title');
     header.style.left = '0px';
     header.style.bottom = '12px';
 }
 
 function setActiveConversationAtIndex(index) {
     //scroll to top of the conversation list
-    var conversationList = document.querySelector("div[class~='pane-list-body']");
+    var conversationList = document.querySelector('div.pane-list-body');
     if (conversationList.scrollTop == 0) {
-        document.querySelector('div:nth-child('+index+')[class="infinite-list-item"]').firstChild.click();
+        document.querySelector('div:nth-child('+index+').infinite-list-item').firstChild.click();
     } else {
         new MutationObserver(function() {
-                             document.querySelector('div:nth-child('+index+')[class="infinite-list-item"]').firstChild.click();
+                             document.querySelector('div:nth-child('+index+').infinite-list-item').firstChild.click();
                              this.disconnect();
                              }).observe(conversationList, {attributes: true, childList: true, subtree: true});
     }
