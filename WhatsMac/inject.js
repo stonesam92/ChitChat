9ca398c7d4cc957f9c3f32668bc7b3cef65fd8cd
Christian Bieg <christianbieg@aol.de>
2015-08-20 14:15:06 +0200

    Fixed some layout issues
    - Drawers will now always be the same size as the ones in the web app
    - Right drawer will no longer cover up the messages area (Issued here: https://github.com/stonesam92/ChitChat/issues/11)

--- WhatsMac/inject.js
+++ WhatsMac/inject.js
@@ -14,15 +14,31 @@ this.Notification.requestPermission = function(callback) {callback('granted');};
 
 
 var styleAdditions = document.createElement('style');
-styleAdditions.textContent = 'div.pane-list-user {opacity:0;} \
+styleAdditions.textContent = 'div.pane-list-user { opacity:0; } \
 div.pane-list-user > div.avatar { width: 0px; height: 0px; } \
-div.app-wrapper::before {opacity: 0;} \
-div.drawer-title {left:60px; bottom:17px;} \
+div.app-wrapper::before { opacity: 0; } \
+div.drawer-title { left:60px; bottom:17px; } \
 div.chat.media-chat > div.chat-avatar { opacity: 0;} \
-div.app.two { top: 0px; width: 100%; height: 100%; } \
-div.app.three { top: 0px; width: 100%; height: 100%; } \
-div.pane.pane-chat { width : 100%; } \
-div.pane.pane-intro { width : 100%; } \
+div.app.three, div.app.two { top: 0px; width: 100%; height: 100%; } \
+@media screen and (max-width:1024px) { \
+    .pane-list, .drawer-container-left { width: 38%; } \
+    .drawer-container-mid, .drawer-container-right, .pane-chat, .pane-intro, .pane-info, .drawer-container-panel { width: 62%; } \
+} \
+@media screen and (min-width:1024px) and (max-width:1199px) { \
+    .pane-list, .drawer-container-left, .pane-info, .drawer-container-panel { width: 30%; } \
+    .drawer-container-mid, .drawer-container-right, .pane-chat, .pane-intro { width: 70%; } \
+    .three .drawer-container-mid, .three .drawer-container-right, .three .pane-chat, .three .pane-intro { width: 40%; } \
+} \
+@media screen and (min-width:1200px) { \
+    .pane-list, .drawer-container-left, .pane-info, .drawer-container-panel { width: 359px; } \
+    .drawer-container-mid, .drawer-container-right, .pane-chat, .pane-intro { width: calc(100% - 359px); } \
+    .three .drawer-container-mid, .three .drawer-container-right, .three .pane-chat, .three .pane-intro { width: calc(100% - 718px); } \
+}\
+@media screen and (min-width:1320px) {\
+    .pane-list, .drawer-container-left, .pane-info, .drawer-container-panel { width: 388px; } \
+    .drawer-container-mid, .drawer-container-right, .pane-chat, .pane-intro { width: calc(100% - 388px); } \
+    .three .drawer-container-mid, .three .drawer-container-right, .three .pane-chat, .three .pane-intro { width: calc(100% - 718px); } \
+}\
 ';
 document.documentElement.appendChild(styleAdditions);
 
