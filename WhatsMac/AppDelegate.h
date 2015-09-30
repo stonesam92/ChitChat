98def52bc62a9fe0d49276121ed72ed5c14524e5
Anders <hello@andersio.co>
2015-09-30 22:11:33 +0800

    Revert unnecessary change to AppDelegate.

--- WhatsMac/AppDelegate.h
+++ WhatsMac/AppDelegate.h
@@ -1,7 +1,7 @@
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSResponder <NSApplicationDelegate>
-@property(readonly) NSWindow* window;
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
 - (void)setActiveConversationAtIndex:(NSString*)index;
 - (BOOL)shouldPropagateMouseUpEvent:(NSEvent*)theEvent;
 - (BOOL)shouldPropagateMouseDownEvent:(NSEvent*)theEvent;
