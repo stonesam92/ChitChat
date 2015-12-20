df5de85ec80012f13dc7c8c3799f01ae61b0e8d1
theld07 <lucadaddabbo@icloud.com>
2015-12-20 15:41:30 +0100

    Added quick reply variable declaration
    Declaring Quick Reply banner variables

--- WhatsMac/AppDelegate.h
+++ WhatsMac/AppDelegate.h
@@ -6,5 +6,15 @@
 - (BOOL)shouldPropagateMouseUpEvent:(NSEvent*)theEvent;
 - (BOOL)shouldPropagateMouseDraggedEvent:(NSEvent*)theEvent;
 
+@property BOOL hasReplyButton NS_AVAILABLE(10_9, NA);
+
+// Optional placeholder for inline reply field.
+@property (copy) NSString *responsePlaceholder NS_AVAILABLE(10_9, NA);
+
+// When a notification has been responded to, the NSUserNotificationCenter delegate
+// didActivateNotification: will be called with the notification with the activationType
+// set to NSUserNotificationActivationTypeReplied and the response set on the response property
+@property (readonly) NSAttributedString *response NS_AVAILABLE(10_9, NA);
+
 @end
 
